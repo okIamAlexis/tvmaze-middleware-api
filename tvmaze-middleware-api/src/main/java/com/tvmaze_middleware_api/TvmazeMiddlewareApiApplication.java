91e64d8226ae72fad78ddb882677ce5dde255771
Alexis Molinero <molineroc@globalhitss.com>
2026-09-10 19:16:05 -0600

    Spring Boot project setup with dependencies

---
+++ tvmaze-middleware-api/src/main/java/com/tvmaze_middleware_api/TvmazeMiddlewareApiApplication.java
@@ -0,0 +1,13 @@
+package com.tvmaze_middleware_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmazeMiddlewareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvmazeMiddlewareApiApplication.class, args);
+	}
+
+}
---
+++ tvmaze-middleware-api/src/test/java/com/tvmaze_middleware_api/TvmazeMiddlewareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvmaze_middleware_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvmazeMiddlewareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
