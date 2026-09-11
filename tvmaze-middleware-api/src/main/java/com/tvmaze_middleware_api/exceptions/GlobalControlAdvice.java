42c5d9b9ed6fd25582592a4ee7171c249b7e6279
Alexis Molinero <molineroc@globalhitss.com>
2026-09-11 10:31:29 -0600

    fix: manejar parametros de request invalidos o faltantes

--- tvmaze-middleware-api/src/main/java/com/tvmaze_middleware_api/exceptions/GlobalControlAdvice.java
+++ tvmaze-middleware-api/src/main/java/com/tvmaze_middleware_api/exceptions/GlobalControlAdvice.java
@@ -8,9 +8,11 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -54,7 +56,31 @@ public class GlobalControlAdvice {
         return problemDetail;
     }
 
-	
+
+	//Bad request - path/query param con tipo invalido (ej. /shows/abc en vez de /shows/169)
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ProblemDetail handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException ex) {
+		log.error("Invalid parameter type: {}", ex.getMessage());
+		String detail = "El valor '" + ex.getValue() + "' no es valido para el parametro '" + ex.getName() + "'";
+		ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, detail);
+		problemDetail.setTitle("Invalid Parameter");
+		problemDetail.setProperty("Timestamp", Instant.now());
+		return problemDetail;
+	}
+
+	//Bad request - falta un parametro requerido (ej. /search/shows sin ?q=)
+	@ExceptionHandler(MissingServletRequestParameterException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ProblemDetail handleMissingServletRequestParameter(MissingServletRequestParameterException ex) {
+		log.error("Missing request parameter: {}", ex.getMessage());
+		String detail = "El parametro requerido '" + ex.getParameterName() + "' no fue enviado";
+		ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, detail);
+		problemDetail.setTitle("Missing Parameter");
+		problemDetail.setProperty("Timestamp", Instant.now());
+		return problemDetail;
+	}
+
 	//Internal_Errors
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleException(Exception ex){
